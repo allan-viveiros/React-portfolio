bba53547503ea60ca4528823fbe34db3741081d2
allan-viveiros <viveirosallan@gmail.com>
2020-07-05 19:21:05 -0400

    adjusts to publish beta version

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import '../sass/about-style.scss';
-import Photo from '../images/Deadpool.jpg';
+import Photo from '../images/Me-2.png';
 import Footer from '../components/footer/footer';
 import Resume from '../documents/Allan_Viveiros.pdf';
 
@@ -16,15 +16,28 @@ const AboutMe = () => (
             <h2 className="sm-heading"> Let me tell you a few things about me.  </h2>
 
             <div className="about-info">
+
                 <img src={Photo} alt="Allan Romero" className="bio-image" />
 
                 <div className="bio">
                     <h3 className="text-secondary blue"> Bio </h3>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Aperiam omnis labore non neque eligendi consequatur odit possimus, 
-                        facilis ullam cupiditate impedit incidunt dicta. Tempora numquam ad 
-                        commodi praesentium beatae consequatur cupiditate </p>
+                    <p>
+                        Oi, Meu nome é Allan Viveiros, sou formado em analise de sistemas com 
+                        especializacao em desenvolvimento em banco de dados, atuo na area de TI a 
+                        aproximadamente 10 anos, nesse tempo trabalhei como analista e desenvolvedor
+                        Oracle e analista de sistemas e negocio. Atualmente residente em London Ontario,
+                        a procura de expandir meus conhecimentos na area de desenvolvimento web, pois 
+                        acredito que essa é a plataforma do presente e uma area em constante expansao
+                        e desenvolvimento. Todo empreendimento de sucesso preecisa ter uma boa 
+                        plataforma online, atraente e intuitiva. Pensando nisso, fui formente atraido 
+                        pela lingugem javaScript, por onde comecei a estudar e praticar e entao cheguei
+                        a ferramentas como React e o Node. Depois de muitas horas de estudo e pratica
+                        posso dizer que estou confortavel em utilizar tecnologias como: HTML5, CSS3, 
+                        Sass, Javascript, React, Node, Hooks, entre outras. É possivel verificar aqui
+                        alguns trabalhos.                        
+                    </p>
+
                 </div>
             
                 <div className="personal">                    
@@ -40,12 +53,14 @@ const AboutMe = () => (
                             Hooks - jQuery - Json
                         </p>  
 
+                        {/*
                         <a href={Resume} target='_blank' rel="noopener noreferrer">    
                             <div className="btn-download"> 
                                 <span> Download Resume </span>
                             </div>
                         </a>
-
+                        */}
+                        
                         <div class="btn-download-container">
                             <a href={Resume} target='_blank' rel="noopener noreferrer">    
                                 <button class="button-border">
@@ -65,36 +80,63 @@ const AboutMe = () => (
                         <div className="timeline-body">   
 
                             <div className="timeline-item">
-                                <p className="time">  2014 - 2019 </p>
+                                <p className="time"> 2019 </p>
                                 <div className="content">
-                                    <h2 className="title"> Business System Analyst </h2>
+                                    <h2 className="title"> Business System Analyst </h2> 
+                                    <p className="sub-text"> Job position 2014 - 2019 </p>
                                     <p> Industria de produtos alimentícios Piraque, Brazil, Rio de Janeiro. 
                                     </p>
                                 </div>
                             </div>
 
                             <div className="timeline-item">
-                                <p className="time">  2010 - 2014 </p>
+                                <p className="time"> 2015 </p>
+                                <div className="content">
+                                    <h2 className="title"> MIT- Data base management </h2>
+                                    <p className="sub-text"> Post-graduate 2013 - 2015 </p>
+                                    <p> Instituto Infet - Brazil, Rio de Janeiro.
+                                        Oracle database management system and others SGBD. 
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="timeline-item">
+                                <p className="time"> 2014 </p>
                                 <div className="content">
                                     <h2 className="title"> Oracle analyst and PL/SQL developer </h2>
-                                    <p> Industria de produtos alimentícios Piraque, Brazil, Rio de Janeiro. 
+                                    <p className="sub-text"> Job position 2010 - 2014 </p>
+                                    <p> Faculdade de informatica Lemos de Castro - Brazil, Rio de Janeiro.
+                                        System analysis and development. 
                                     </p>
                                 </div>
                             </div>
 
                             <div className="timeline-item">
-                                <p className="time">  2006 - 2009 </p>
+                                <p className="time"> 2011 </p>
+                                <div className="content">
+                                    <h2 className="title"> Information Systems </h2>
+                                    <p className="sub-text"> Bachelor degree 2006 - 2011 </p>
+                                    <p> Instituto Infet - Brazil, Rio de Janeiro.
+                                        Oracle database management system and others SGBD. 
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="timeline-item">
+                                <p className="time"> 2009 </p>
                                 <div className="content">
                                     <h2 className="title"> Sales assistant </h2>
+                                    <p className="sub-text"> Job position 2006 - 2009 </p>
                                     <p> Industria de produtos alimentícios Piraque, Brazil, Rio de Janeiro. 
                                     </p>
                                 </div>
                             </div>
 
                             <div className="timeline-item">
-                                <p className="time">  2005 - 2006 </p>
+                                <p className="time"> 2006 </p>
                                 <div className="content">
                                     <h2 className="title"> Administrative auxiliary </h2>
+                                    <p className="sub-text"> Job position 2005 - 2006 </p>
                                     <p> Industria de produtos alimentícios Piraque, Brazil, Rio de Janeiro. 
                                     </p>
                                 </div>
@@ -103,30 +145,8 @@ const AboutMe = () => (
                         </div>
                     </div>
                 
-                </div>
+                </div>                
                 
-                <div className="job job1">
-                    <h3> MIT- Data base management </h3>
-                    <h6> Post-graduate 2013 - 2015 </h6>
-                    <p> Instituto Infet - Brazil, Rio de Janeiro.</p>
-                    <p> Oracle database management system and others SGBD.</p>
-                </div>
-
-                <div className="job job2">
-                    <h3> Information Systems </h3>
-                    <h6> Bachelor degree 2006 - 2011 </h6>
-                    <p> Faculdade de informatica Lemos de Castro - Brazil, Rio de Janeiro. </p>
-                    <p> System analysis and development. </p>
-                </div>
-
-                <div className="job job3">
-                    <h3> Job 3 </h3>
-                    <h6> Job position </h6>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus 
-                        eum fugit architecto asperiores illo nihil impedit eaque quibusdam 
-                        ducimus tempora.
-                    </p>
-                </div>
 
             </div>
 
--- src/pages/my-works.jsx
+++ src/pages/my-works.jsx
@@ -4,9 +4,22 @@ import '../sass/myWorks-style.scss';
 import Card from '../components/cards/cards';
 import Footer from '../components/footer/footer';
 
+//Projects
 import SuperClothing from '../images/projects/super-clothing.PNG';
 import Monster from '../images/projects/monster.PNG';
 import Covid from '../images/projects/covid-tracker.PNG';
+
+//Apps
+import StopWatch from '../images/projects/stopWatch.PNG';
+import Drawing from '../images/projects/drawing.PNG';
+import MathsGame from '../images/projects/mathsGame.PNG'
+import Fruits from '../images/projects/Fruits.PNG';
+import Breakout from '../images/projects/breakout.PNG';
+
+//Templates
+import Landing from '../images/projects/landing.PNG';
+import Lovely from '../images/projects/lovely.PNG';
+import Awesome from '../images/projects/awesome.PNG';
 import Maths from '../images/projects/maths.PNG';
 
 
@@ -20,6 +33,11 @@ const MyWorks = () => (
             </h1>
             <h2 className="sm-heading"> Check out some of my projects.  </h2>
             
+            <h2 className="title-projects"> 
+                Main Projects 
+                <p> Some projects developed using React </p>
+            </h2>
+
             <div className="main-projects"> 
 
                 {/* Super Clothing */}
@@ -64,84 +82,160 @@ const MyWorks = () => (
                     linkProject="https://covid19-tckr.herokuapp.com/"
                     linkGithub="https://github.com/allan-viveiros/covid19-tracker"                    
                 />
+            </div>            
 
-                {/* Maths */}
+            <h2 className="title-projects"> 
+                Games Js 
+                <p> Some games developed using pure JavaScript and bootstrap </p>
+            </h2>
+
+            <div className="main-projects">
+
+                {/* StopWatch */} 
                 <Card 
-                    imgProject={Maths}
-                    imgAlt="Maths"
-                    project="Frontend Development"
-                    title="Maths"
-                    textFront="Website using only HTML5 CSS3."
-                    textBack="Responsive website using HTML5, CSS3."
-                    publish=""
-                    linkProject="https://super-clothing.herokuapp.com/"
-                    linkGithub="https://github.com/allan-viveiros/super-clothing"                    
+                    imgProject={StopWatch}
+                    imgAlt="StopWatch"
+                    project="Frontend JavaScript app"
+                    title="StopWatch app"
+                    textFront="Stopwatch app with lap count"
+                    textBack="Responsive app using HTML, CSS, JavaScript and bootstrap"
+                    publish="Publish on GitHub"
+                    linkProject="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/7.Stopwatch%20App/StopWatch.html"
+                    linkGithub="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/7.Stopwatch%20App"                    
                 />
 
-                {/*  */}
+                {/* Drawing */} 
                 <Card 
-                    imgProject={SuperClothing}
-                    imgAlt="Super Clothing"
-                    project="e-Commerce Project"
-                    title="Super Clothing"
-                    textFront="Website using firebase, 
-                        google authentication and integrated with stripe system 
-                        to make payments."
-                    textBack="Responsive website using HTML5, Sass, JavaScript, React, React Hooks, 
-                        Firebase."
-                    publish="Publish on Heroku"
-                    linkProject="https://super-clothing.herokuapp.com/"
-                    linkGithub="https://github.com/allan-viveiros/super-clothing"                    
+                    imgProject={Drawing}
+                    imgAlt="Drawing app"
+                    project="Frontend JavaScript app"
+                    title="Drawing app"
+                    textFront="Make colorized Drawings and save"
+                    textBack="Drawing app using HTML, CSS, JavaScript, Jquery, bootstrap and local store"
+                    publish="Publish on GitHub"
+                    linkProject="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/8.Drawing%20App/Drawing.html"
+                    linkGithub="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/8.Drawing%20App"                    
+                />
+                
+                {/* Maths Game */} 
+                <Card 
+                    imgProject={MathsGame}
+                    imgAlt="Maths game"
+                    project="JavaScript game"
+                    title="Maths Game"
+                    textFront="Quiz game. Choose the correct answer and make points"
+                    textBack="Quiz app using HTML, CSS and JavaScript"
+                    publish="Publish on GitHub"
+                    linkProject="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/2.Maths%20Game/MathGame.html"
+                    linkGithub="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/2.Maths%20Game"                    
+                />
+
+                {/* Fruit Game */} 
+                <Card 
+                    imgProject={Fruits}
+                    imgAlt="Cut fruits game"
+                    project="JavaScript game"
+                    title="Fruits Game"
+                    textFront="Cut the fruits and make points"
+                    textBack="Cut fruits game using HTML, CSS, JavaScript and Jquery"
+                    publish="Publish on GitHub"
+                    linkProject="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/3.Fruits%20Game/FruitGame.html"
+                    linkGithub="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/3.Fruits%20Game"                    
+                />
+
+                {/* Breakout Game */} 
+                <Card 
+                    imgProject={Breakout}
+                    imgAlt="Breakout game"
+                    project="JavaScript game"
+                    title="Breakout Game"
+                    textFront="Using the ball to Break the bricks and make points"
+                    textBack="breakout game using HTML, CSS, JavaScript"
+                    publish="Publish on GitHub"
+                    linkProject="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/BreakOut_Game/BreakOut.html"
+                    linkGithub="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/BreakOut_Game"                    
                 />
 
 
-<Card 
-                    imgProject={SuperClothing}
-                    imgAlt="Super Clothing"
-                    project="e-Commerce Project"
-                    title="Super Clothing"
-                    textFront="Website using firebase, 
-                        google authentication and integrated with stripe system 
-                        to make payments."
-                    textBack="Responsive website using HTML5, Sass, JavaScript, React, React Hooks, 
-                        Firebase."
-                    publish="Publish on Heroku"
-                    linkProject="https://super-clothing.herokuapp.com/"
-                    linkGithub="https://github.com/allan-viveiros/super-clothing"                    
-                />
+            </div>
+          
+            
 
+            <h2 className="title-projects"> 
+                Templates HTML 
+                <p> Some templates developed using HTML, CSS, and bootstrap </p>
+            </h2>
 
-<Card 
-                    imgProject={SuperClothing}
-                    imgAlt="Super Clothing"
-                    project="e-Commerce Project"
-                    title="Super Clothing"
-                    textFront="Website using firebase, 
-                        google authentication and integrated with stripe system 
-                        to make payments."
-                    textBack="Responsive website using HTML5, Sass, JavaScript, React, React Hooks, 
-                        Firebase."
-                    publish="Publish on Heroku"
-                    linkProject="https://super-clothing.herokuapp.com/"
-                    linkGithub="https://github.com/allan-viveiros/super-clothing"                    
-                />
+            <div className="main-projects">
 
+                {/* Landing page */}
+                <div className="item">                    
+                    <img className="template-img" src={Landing} alt="Landing page" />
+                    
+                    <a href="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/4.Landing%20page/LandinPage.html" 
+                     className="btn-light" rel="noopener noreferrer" target="_blank">
 
-<Card 
-                    imgProject={SuperClothing}
-                    imgAlt="Super Clothing"
-                    project="e-Commerce Project"
-                    title="Super Clothing"
-                    textFront="Website using firebase, 
-                        google authentication and integrated with stripe system 
-                        to make payments."
-                    textBack="Responsive website using HTML5, Sass, JavaScript, React, React Hooks, 
-                        Firebase."
-                    publish="Publish on Heroku"
-                    linkProject="https://super-clothing.herokuapp.com/"
-                    linkGithub="https://github.com/allan-viveiros/super-clothing"                    
-                />
+                        <i className="fas fa-eye"></i> Project
+                    </a>             
 
+                    <a href="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/4.Landing%20page" 
+                     className="btn-dark" rel="noopener noreferrer" target="_blank">
+
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                </div>
+
+                {/* Awesome page */}
+                <div className="item">                    
+                    <img className="template-img" src={Awesome} alt="Awesome page" />
+                    
+                    <a href="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/5.Website/Awesome.html" 
+                     className="btn-light" rel="noopener noreferrer" target="_blank">
+
+                        <i className="fas fa-eye"></i> Project
+                    </a>          
+
+                    <a href="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/5.Website" 
+                     className="btn-dark" rel="noopener noreferrer" target="_blank">
+
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                </div>
+
+                {/* Lovely page */}
+                <div className="item">                    
+                    <img className="template-img" src={Lovely} alt="Lovely page" />
+                    
+                    <a href="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/6.Lovely%20Website/Lovely.html" 
+                     className="btn-light" rel="noopener noreferrer" target="_blank">
+
+                        <i className="fas fa-eye"></i> Project
+                    </a>          
+
+                    <a href="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/6.Lovely%20Website" 
+                     className="btn-dark" rel="noopener noreferrer" target="_blank">
+
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                </div>
+
+                {/* Maths page */}
+                <div className="item">                    
+                    <img className="template-img" src={Maths} alt="Maths page" />
+                    
+                    <a href="https://allan-viveiros.github.io/Portfolio-allan/frontend-projects/1.Maths%20Website/Maths.html" 
+                     className="btn-light" rel="noopener noreferrer" target="_blank">
+
+                        <i className="fas fa-eye"></i> Project
+                    </a>          
+
+                    <a href="https://github.com/allan-viveiros/Portfolio-allan/tree/master/src/frontend-projects/1.Maths%20Website" 
+                     className="btn-dark" rel="noopener noreferrer" target="_blank">
+
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                </div>
+                
             </div>
 
         </main>
